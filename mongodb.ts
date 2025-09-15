da2ef7a60e6604bd849ebf7222f2e7d447c670f0
dvoytenko <dima.voytenko@vercel.com>
2025-09-15 16:56:08 -0700

    Retry reads

--- mongodb.ts
+++ mongodb.ts
@@ -4,6 +4,7 @@ import { attachDatabasePool } from "@vercel/functions";
 const uri = process.env.MONGODB_URI;
 const options: MongoClientOptions = {
   appName: "devrel.vercel.integration",
+  retryReads: true,
 };
 
 let client: MongoClient;
